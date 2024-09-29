da8a5fd8ec83ac16242174459d0b045cf62db5a0
RavinduICIEOS <ravindu@icieos.com>
2024-09-30 04:59:00 +0530

    test_1

---
+++ utils/authorization.js
@@ -0,0 +1,18 @@
+//const express = require('express')/////////////////////
+
+function checkAuthorization(req,res,next) {
+
+    if(req.session) {
+
+        if(req.session.user) {
+            //res.locals.authenticated = true////////////////////////
+            next()
+        }else {
+            res.redirect('/login')
+        }
+    }else{
+        res.redirect('/login')
+    }     
+}
+
+module.exports = checkAuthorization
